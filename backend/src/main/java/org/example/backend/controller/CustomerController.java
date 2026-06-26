d778b096c6102021997d360ace28397dacef2e27
huy2222 <nguyennhathuy21112005@gmail.com>
2026-06-26 11:41:10 +0700

    tinh nang up hinh profile

--- backend/src/main/java/org/example/backend/controller/CustomerController.java
+++ backend/src/main/java/org/example/backend/controller/CustomerController.java
@@ -1,18 +1,22 @@
 package org.example.backend.controller;
 
 
+import lombok.RequiredArgsConstructor;
+import org.example.backend.entity.Customer;
+import org.example.backend.service.CloudinaryService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.example.backend.dto.response.CustomerResponse;
 import org.example.backend.service.CustomerService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/customers")
+@RequiredArgsConstructor
 public class CustomerController {
     private final CustomerService customerService;
-    public CustomerController(CustomerService customerService) {
-        this.customerService = customerService;
-    }
+    private final CloudinaryService cloudinaryService;
 
     @GetMapping("/me")
     public CustomerResponse getMyProfile(Authentication auth) {
@@ -25,6 +29,18 @@ public class CustomerController {
         String email = auth.getName();
         return customerService.updateCustomerProfile(email, updatedProfile);
     }
+
+    @PutMapping("/me/avatar")
+    public CustomerResponse updateAvatar(
+            Authentication auth,
+            @RequestParam MultipartFile avatar
+    ) {
+        String email = auth.getName();
+        return customerService.updateAvatar(email, avatar);
+    }
+
+
+
 
 
 
--- frontend/src/services/customerService.js
+++ frontend/src/services/customerService.js
@@ -2,3 +2,10 @@ import api from "../api/axiosConfig";
 
 export const getProfile = () => api.get(`/customers/me`);
 export const updateProfile = (profileData) => api.put(`/customers/me`, profileData);
+export const updateAvatar = (avatarFile) => {
+  const formData = new FormData();
+  formData.append("avatar", avatarFile);
+  return api.put(`/customers/me/avatar`, formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+};
